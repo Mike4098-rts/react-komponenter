fdc53f7801fdebbfc6e09789e9f0f7bc5bc61a48
Mike <U10604@rts-365.dk>
2022-05-19 11:02:22 +0200

    fetched data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import SectionList from "./Sections";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/sections")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
+  return <div className="Header">{data && <SectionList data={data} />}</div>;
 }
 
 export default App;
---
+++ src/Sections.jsx
@@ -0,0 +1,13 @@
+const SectionList = ({ data }) => {
+  return (
+    <div className="section__list">
+      {data.map((section) => (
+        <div className="header__data" key={section.id}>
+          <h1>{section.title}</h1>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SectionList;
